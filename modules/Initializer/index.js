30566d0744bc08ddf4c163b380d2b333a3e7b18e
AjandM <aj.mardalizad@gmail.com>
2020-04-23 19:43:58 +0430

    simorq boilerplated :tada:

---
+++ modules/Initializer/index.js
@@ -0,0 +1,24 @@
+/***
+ * 
+ * The initializer is the glue between different piece of Simorq Blockchain
+ * 
+ * The current version of simorq initializers needed 10 main module
+ * 
+ * BlockHandler
+ * Client
+ * ConfigHandler
+ * Consensus
+ * ContractCompiler
+ * StateHandler
+ * TranscationProcessor
+ * TypeCreator
+ * Interfaces
+ * Contracts
+ * FilesPath
+ * 
+ * 
+ * Each initializer will return a function which will be use start blockchain by a script
+ * 
+ * Client can be used outside of blockchain full node to interact with blockchain interfaces
+ * 
+ */
